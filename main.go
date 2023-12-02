64f7344984a4459e183716e5db9936b16ea52473
JonahPeck <PeckJonahC@gmail.com>
2023-12-01 22:53:17 -0500

    working on random assignment

--- main.go
+++ main.go
@@ -3,3 +3,21 @@
 // each person can add books to the overall list
 //brainstorm
 //need to work on
+//randomly assigns a person someone else for the gift exchange
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	i := 1
+	members := [7]string{"Jonah", "Maile", "Theo", "MB", "Mark", "Miah", "Abby"}
+	for i < len(members) {
+		fmt.Println(rand.Intn(len(members)))
+		fmt.Println(members[i])
+		i++
+	}
+}
