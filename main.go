84ce88aa21d379ee559d9f1b4431daf09d37de22
JonahPeck <PeckJonahC@gmail.com>
2023-12-09 20:37:24 -0800

    verbs

--- main.go
+++ main.go
@@ -5,7 +5,8 @@ package main
 import "fmt"
 
 func main() {
-	const name = "Jonah"
-	const age = 25
-	fmt.Printf("%v is %v years old \t and the type is %T and %T", name, age, name, age)
+	fmt.Printf("😍")
+	fmt.Println(``)
 }
+
+//you like dog
