561832862c6438d84639ff179e84a2b1890a88ce
JonahPeck <PeckJonahC@gmail.com>
2023-12-13 20:58:35 -0500

    hex

--- main.go
+++ main.go
@@ -5,13 +5,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("😍")
-	fmt.Println(``)
 
-	var g int = 55
+	// var g int = 55
+	J := 5
+
 	//Declare a Variable to hold a value of a certain type
 	//then assign a value of that type to the variable initializing a variable
 	//initializing a variable
-	fmt.Println(g)
+	fmt.Printf("printing as hex, %x ", J)
+	fmt.Printf("printing as binary, %b ", J)
 
+	//zero value is usually used when var is declared
+	//use shortcut to assign variables typically - short declaration operator
+
+	//numeral systems -hexadecimal is actually super cool
 }
